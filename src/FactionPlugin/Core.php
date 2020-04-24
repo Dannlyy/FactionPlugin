b727889fcd6d9bab55abf9c346628d4656bd2d6d
russ14n-fs0c13ty <55710811+russ14n-fs0c13ty@users.noreply.github.com>
2020-04-24 20:36:42 +0100

    Update Core.php
    Basic Main FIle

--- src/FactionPlugin/Core.php
+++ src/FactionPlugin/Core.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace FactionPlugin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as Color;
+
+class FactionPlugin extends PluginBase {
+
+	public function onEnable()
+	{
+		$this->getLogger()->info(Color::DARK_GREEN . " enabled !");
+	}
+
+	public function onDisable()
+	{
+		$this->getLogger()->info(Color::DARK_RED . " disabled !");
+	}
+
+}
