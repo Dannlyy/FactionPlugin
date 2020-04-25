35bd3d750b3294ef61ca663f7abd774a0d379830
BloodyMC <bloodyqsr@gmail.com>
2020-04-25 03:25:46 +0200

    Ajout de la commande de Faction

--- src/FactionPlugin/commands/FactionCommand.php
+++ src/FactionPlugin/commands/FactionCommand.php
@@ -3,11 +3,14 @@
 namespace FactionPlugin\commands;
 
 
+use FactionPlugin\Core;
 use pocketmine\command\PluginCommand;
 
 class FactionCommand extends PluginCommand
 {
-
-
-
+    public function __construct(Core $plugin)
+    {
+        parent::__construct("f", $plugin);
+        $this->setDescription("Cette commande permet d'avoir accés au commandes de Faction.");
+    }
 }
